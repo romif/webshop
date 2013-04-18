ea85cf11ae175f54d95dc019afc3f4c79858a081
unknown <romif@yandex.ru>
2013-04-18 10:28:28 +0300

    WebShop

--- src/main/java/servlet/LogServlet.java
+++ src/main/java/servlet/LogServlet.java
@@ -21,39 +21,14 @@ public class LogServlet extends HttpServlet {
   }
  
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-  
-   
-    
-    
-
-    
- 
-    /*response.setContentLength((int) file.length());
-    response.setContentType(new MimetypesFileTypeMap().getContentType(file));*/
-    
- 
-    //OutputStream output = response.getOutputStream();
-   /* byte[] bytes = new byte[BUFFER_LENGTH];
-    int read = 0;
-    while (read != -1) {
-      read = input.read(bytes, 0, BUFFER_LENGTH);
-      if (read != -1) {
-        output.write(bytes, 0, read);
-        output.flush();
-      }
-    }*/
 	File file = new File(System.getenv("OPENSHIFT_DATA_DIR") + "Logs");
     BufferedReader br=new BufferedReader(new FileReader (file));
     response.setContentType("text/html;charset=utf-8");
     PrintWriter out = response.getWriter();
     String st;
-    while ((st=br.readLine())!=null){out.println(st);out.println("<br/>");}
+    while ((st=br.readLine())!=null)out.println(new java.util.Date()+" "+st+"<br/>");
     out.close();
     br.close();
-
-	  
-    //input.close();
-    //output.close();
   }
  
  
