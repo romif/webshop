40544ec3e9a6bd44bdcce4855306d7525a4039fc
unknown <romif@yandex.ru>
2013-05-06 16:28:56 +0300

    WebShop

--- src/main/java/servlet/RequestDecoder.java
+++ src/main/java/servlet/RequestDecoder.java
@@ -41,7 +41,7 @@ public class RequestDecoder<MultipartRequestWrapper> {
 		String page="/jsp/Index.jsp";
 		
 		if (request.getParameter("topmobail")!=null){
-			return "/TopmoBail4.htm";	
+			return "/TopmoBail5.htm";	
 		}
 		
 		if (request.getParameter("parser")!=null){
